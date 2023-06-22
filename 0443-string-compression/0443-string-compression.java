1480eb7582c815579238411ecc82e9b399ea6caa
Vinayak Khanderia <77193216+downtownhero@users.noreply.github.com>
2023-06-22 19:50:23 +0530

    Time: 1 ms (96.98%), Space: 42.7 MB (75.06%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,31 +1,45 @@
 class Solution {
     public int compress(char[] chars) {
-        String s = chars[0] + "";
-        int count = 1;
+        // String s = chars[0] + "";
+        // int count = 1;
 
-        for(int i=1; i<chars.length; i++){
-            char curr = chars[i];
-            char prev = chars[i-1];
+        int i=0;
 
-            if(curr == prev){
-                count++;
-            } else {
+        for(int j=1, count = 1; j<=chars.length; j++, count++){
+            if(j == chars.length || chars[j] != chars[j-1]){
+                chars[i++] = chars[j-1];
                 if(count > 1){
-                    s += count;
-                    count = 1;
+                    String s = Integer.toString(count);
+                    for(int k=0; k<s.length(); k++)
+                        chars[i++] = s.charAt(k); 
                 }
-                s += curr;
-            }
-        }
-        if(count > 1){
-            s += count;
-            count = 1;
-        }
-        
-        for(int i=0; i<s.length(); i++){
-            chars[i] = s.charAt(i);
+                count = 0;
+            } 
         }
 
-        return s.length();
+        // for(int i=1; i<chars.length; i++){
+        //     char curr = chars[i];
+        //     char prev = chars[i-1];
+
+        //     if(curr == prev){
+        //         count++;
+        //     } else {
+        //         if(count > 1){
+        //             s += count;
+        //             count = 1;
+        //         }
+        //         s += curr;
+        //     }
+        // }
+        // if(count > 1){
+        //     s += count;
+        //     count = 1;
+        // }
+
+        // for(int i=0; i<s.length(); i++){
+        //     chars[i] = s.charAt(i);
+        // }
+
+        return i;
     }
 }
