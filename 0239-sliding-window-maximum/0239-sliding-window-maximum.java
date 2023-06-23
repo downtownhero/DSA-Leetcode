7602ed752280deac1e898bbe00bba9250f2a4b4c
Vinayak Khanderia <77193216+downtownhero@users.noreply.github.com>
2023-06-23 14:41:35 +0530

    Time: 34 ms (92.19%), Space: 60.3 MB (50.42%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -8,7 +8,7 @@ class Solution {
         st.push(n-1);
 
         for(int i=n-2; i>=0; i--){
-            while(st.size() != 0 && nums[i] > nums[st.peek()]){
+            while(st.size() != 0 && nums[i] >= nums[st.peek()]){
                 st.pop();
             }
 
